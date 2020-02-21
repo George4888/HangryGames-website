786fa92fcb95337b75c3021f88165907f04ce41d
George4888 <DanGeorge4888@gmail.com>
2020-02-21 17:34:56 +0200

    change js

--- JS/generateLetters.js
+++ JS/generateLetters.js
@@ -35,34 +35,11 @@ const letters = document
 for (const element of letters) {
   element.addEventListener("click", function() {
     const elementVal = element.innerText;
+    console.log(elementVal);
     if (elementVal) {
       generateUrl(
         `https://www.themealdb.com/api/json/v1/1/search.php?f=${elementVal}`
       );
     }
   });
-}
-
-const renderContent = document.getElementById("renderContent");
-
-function displayMeal() {
-  console.log("a");
-  renderContent.innerHTML = null;
-  for (const meal of meals) {
-    const mealContainer = document.createElement("div");
-    mealContainer.classList.add("single-meal");
-    renderContent.appendChild(mealContainer);
-
-    const mealTitle = document.createElement("h3");
-    mealTitle.innerText = meal.strMeal;
-    mealContainer.appendChild(mealTitle);
-
-    const mealImg = document.createElement("img");
-    mealImg.setAttribute("src", meal.strMealThumb);
-    mealContainer.appendChild(mealImg);
-
-    const mealContent = document.createElement("p");
-    mealContent.innerText = meal.strInstructions;
-    mealContainer.appendChild(mealContent);
-  }
 }
